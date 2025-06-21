65a616dc3a396dd558a7b678ff1d8053bad322e2
edaakyil <edaakyil@gmail.com>
2025-06-21 15:02:31 +0300

    Update

--- Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
+++ Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/DateDisplay.java
@@ -3,10 +3,20 @@ package com.edaakyil.java.app.component;
 import com.karandev.io.util.console.Console;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class DateDisplay {
     public DateDisplay()
     {
         Console.writeLine("I am a default ctor of DateDisplay");
+    }
+
+    public void displayLocalDate()
+    {
+        var today = LocalDate.now();
+
+        Console.writeLine("DateDisplay: %s", today.format(DateTimeFormatter.ISO_DATE));
     }
 }
--- Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/TimeDisplay.java
+++ Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/TimeDisplay.java
@@ -4,11 +4,21 @@ import com.karandev.io.util.console.Console;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @Lazy
 public class TimeDisplay {
     public TimeDisplay()
     {
         Console.writeLine("I am a default ctor of TimeDisplay");
+    }
+
+    public void displayLocalTime()
+    {
+        var now = LocalTime.now();
+
+        Console.writeLine("TimeDisplay: %s", now.format(DateTimeFormatter.ISO_TIME));
     }
 }
