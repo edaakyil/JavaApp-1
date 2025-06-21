fe7edde5233c255b276c65643ef2e542b6ffc8ac
edaakyil <edaakyil@gmail.com>
2025-06-21 15:28:18 +0300

    Update

---
+++ Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/configuration/DateTimeFormatterConfig.java
@@ -0,0 +1,15 @@
+package com.edaakyil.java.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class DateTimeFormatterConfig {
+    @Bean
+    public DateTimeFormatter createDateTimeFormatter()
+    {
+        return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    }
+}
