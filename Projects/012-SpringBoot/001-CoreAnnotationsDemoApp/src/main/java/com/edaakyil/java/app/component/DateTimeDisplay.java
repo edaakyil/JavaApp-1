7e26cb3988de5d0973b2876c853c06b1be17cd17
edaakyil <edaakyil@gmail.com>
2025-06-21 14:11:35 +0300

    Update

--- Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/DateTimeDisplay.java
+++ Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/DateTimeDisplay.java
@@ -1,9 +1,11 @@
 package com.edaakyil.java.app.component;
 
 import com.karandev.io.util.console.Console;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class DateTimeDisplay {
     public DateTimeDisplay()
     {
