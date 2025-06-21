22c27a59ab92d119bf53fef8d511f0800b8fe2a1
edaakyil <edaakyil@gmail.com>
2025-06-21 14:02:17 +0300

    Update

---
+++ Projects/012-SpringBoot/001-CoreAnnotationsDemoApp/src/main/java/com/edaakyil/java/app/component/DateTimeDisplay.java
@@ -0,0 +1,10 @@
+package com.edaakyil.java.app.component;
+
+import com.karandev.io.util.console.Console;
+
+public class DateTimeDisplay {
+    public DateTimeDisplay()
+    {
+        Console.writeLine("I am a default ctor of DateTimeDisplay");
+    }
+}
