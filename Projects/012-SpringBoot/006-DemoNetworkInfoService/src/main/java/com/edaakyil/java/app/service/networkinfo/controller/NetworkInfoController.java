4f6e66eac3e2c8adf2ebdc254ceb87af0924532d
edaakyil <edaakyil@gmail.com>
2025-08-20 14:14:59 +0300

    Update

--- Projects/012-SpringBoot/006-DemoNetworkInfoService/src/main/java/com/edaakyil/java/app/service/networkinfo/controller/NetworkInfoController.java
+++ Projects/012-SpringBoot/006-DemoNetworkInfoService/src/main/java/com/edaakyil/java/app/service/networkinfo/controller/NetworkInfoController.java
@@ -1,7 +1,11 @@
 package com.edaakyil.java.app.service.networkinfo.controller;
 
+import com.edaakyil.java.app.service.networkinfo.dto.NetworkInfo;
+import com.karandev.datetime.configuration.constant.BeanName;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +16,12 @@ import java.time.LocalDateTime;
 @RequestMapping("/info/network")
 @Scope("prototype")
 @Slf4j
+@ComponentScan("com.karandev")
 public class NetworkInfoController {
-    private final LocalDateTime m_localDateTime;
     private final HttpServletRequest m_request;
+    private final LocalDateTime m_localDateTime;
 
-    public NetworkInfoController(LocalDateTime localDateTime, HttpServletRequest request)
+    public NetworkInfoController(@Qualifier(BeanName.LOCAL_CURRENT_DATETIME_BEAN) LocalDateTime localDateTime, HttpServletRequest request)
     {
         m_localDateTime = localDateTime;
         m_request = request;
